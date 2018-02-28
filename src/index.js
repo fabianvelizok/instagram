392636c31bb6f7be00f3e60be31626d1146cd8df
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-02-28 02:38:48 -0300

    Add page.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+var page = require('page');
+var main = document.getElementById('main-container');
 
+page("/", function(context, next) {
+  var html = 'Home <a href="/Signup">Signup</a>';
+  main.innerHTML = html;
+});
+
+page("/signup", function(context, next) {
+  var html = 'Signup <a href="/">Home</a>';
+  main.innerHTML = html;
+});
+
+page();
